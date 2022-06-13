0c149c6854c31dea9bb9f26cd9f2153a8fe84c18
eriksonsantos <eriksoncode@gmail.com>
2022-06-13 19:29:44 -0300

    change function CreateThread to _beginthreadex

--- oil-company/oil-company.cpp
+++ oil-company/oil-company.cpp
@@ -5,58 +5,59 @@
 #include <Windows.h>
 #include "DataSCADA.h"
 #include "DataAlarm.h"
+#include <process.h>
 
-
-DWORD WINAPI ThreadSCADA(LPVOID index);
-DWORD WINAPI ThreadAlarm(LPVOID index);
-DWORD WINAPI ThreadOptimization(LPVOID index);
+unsigned __stdcall  ThreadSCADA(LPVOID index);
+unsigned __stdcall  ThreadAlarm(LPVOID index);
+unsigned __stdcall ThreadOptimization(LPVOID index);
 
 int main()
-{   
-
+{ 
     srand((unsigned)time(0));
     
-   
     HANDLE hThreads[3];
-    DWORD dwThreadId;
+    unsigned dwThreadId;
 
     hThreads[0] = (HANDLE)
-            CreateThread(NULL, 0, ThreadOptimization, (LPVOID)0, 0, &dwThreadId);
+        _beginthreadex(NULL, 0, &ThreadOptimization, (LPVOID)0, 0, &dwThreadId);
    hThreads[1] = (HANDLE)
-        CreateThread(NULL, 0, ThreadAlarm, (LPVOID)1, 0, &dwThreadId);
+       _beginthreadex(NULL, 0, ThreadAlarm, (LPVOID)1, 0, &dwThreadId);
    hThreads[2] = (HANDLE)
-       CreateThread(NULL, 0, ThreadSCADA, (LPVOID)2, 0, &dwThreadId);
+       _beginthreadex(NULL, 0, ThreadSCADA, (LPVOID)2, 0, &dwThreadId);
    
    WaitForSingleObject(hThreads[0], INFINITE);
    WaitForSingleObject(hThreads[1], INFINITE);
    WaitForSingleObject(hThreads[2], INFINITE);
 
+
    return 0;
 }
-DWORD WINAPI ThreadOptimization(LPVOID index) {
+unsigned __stdcall  ThreadOptimization(LPVOID index) {
     dataOptimization data;
     while (1) {
         cout << "Optimization: " + data.GenerateData() << endl;
         Sleep(1000);
     }
+    CloseHandle(index);
     return 0;
 }
-DWORD WINAPI ThreadAlarm(LPVOID index) {
+unsigned __stdcall  ThreadAlarm(LPVOID index) {
     dataAlarm data;
     while (1) {
         cout << "Alarme: " + data.GenerateData() << endl;
         Sleep(1000);
     }
-
+    CloseHandle(index);
     return 0;
 
 }
-DWORD WINAPI ThreadSCADA(LPVOID index) {
+unsigned __stdcall  ThreadSCADA(LPVOID index) {
     dataSCADA data;
     while (1) {
         cout << "SCADA: " + data.GenerateData() << endl;
         Sleep(1000);
     }
+    CloseHandle(index);
 
     return 0;
 
